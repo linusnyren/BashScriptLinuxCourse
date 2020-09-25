352808132dfd8482760416297837701cec55ab7d
linusnyren <linusny@hotmail.com>
2020-09-25 10:38:04 +0200

    Removed

--- uppgift2_3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Install sshpass to enable automatic password filling
-sudo apt install sshpass -y
-
-#ssh into the server using the sshpass command
-sshpass -p "password" ssh -o StrictHostKeyChecking=no server@192.168.0.67
-
-
